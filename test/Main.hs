bc2e9378ebb58f66903b0b208f871801f9cfd93b
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-03 15:12:34 +0100

    works!

--- test/Main.hs
+++ test/Main.hs
@@ -1,35 +1,36 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE UndecidableInstances #-}
+
 module Main (main) where
 
-import GHC.Records
 import GHC.Generics
 import Optics.Core
 import Optics.Lens.Dot
-import GHC.TypeLits
 
-data Whole a = Whole {
-    whole1 :: Int, 
+data Whole a = Whole
+  { whole1 :: Int,
     part :: Part a
-} deriving stock Generic
+  }
+  deriving stock (Generic)
 
-data Part a = Part {
-    part1 :: Bool,
-    subpart :: Subpart a  
-} deriving stock Generic
+data Part a = Part
+  { part1 :: Bool,
+    subpart :: Subpart a
+  }
+  deriving stock (Generic)
 
-data Subpart a = Subpart {
-    subpart1 :: String,
+data Subpart a = Subpart
+  { subpart1 :: String,
     foo :: a
-} deriving stock Generic
+  }
+  deriving stock (Generic)
 
 whole :: Whole Int
 whole = Whole 0 (Part True (Subpart "wee" 7))
 
 whole' :: Whole Bool
-whole' = whole & the.part.subpart.foo .~ False 
+whole' = whole & the.part.subpart.foo .~ False
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
