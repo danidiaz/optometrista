1522478df06cf1a5277bde427cc65c9889aaa713
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-03 16:14:28 +0100

    RecordDotOptics

--- lib/Optics/Lens/Dot.hs
+++ lib/Optics/Lens/Dot.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Optics.Lens.Dot where
 
 import GHC.Records
@@ -8,6 +9,7 @@ import Optics.Core
 import Optics.Coerce
 import GHC.TypeLits
 import Data.Proxy
+import Data.Kind
     
 instance 
     (RecordDotOptics name u v a b,
@@ -15,20 +17,20 @@ instance
      AppendIndices is NoIx ks)
     =>
     HasField name (Optic k is s t u v) (Optic m ks s t a b) where
-        getField o = o % theOptic (Proxy @name)
+        getField o = o % theOptic @name
 
 the :: Iso a b a b
 the = Optics.Core.equality
 
-class RecordDotOptics name u v a b |  name u -> v a b, name v -> u a b where
-    theOptic :: Proxy name -> Lens u v a b
+class RecordDotOptics name u v a b | name u -> v a b, name v -> u a b where
+    theOptic :: Lens u v a b
 
-newtype GenericOptics r = GenericOptics r
+-- newtype GenericOptics r = GenericOptics r
 
 -- https://stackoverflow.com/questions/53009549/haskell-derivingvia-on-multi-param-type-classes-with-fun-deps
 
-instance GField name u v a b => RecordDotOptics name (GenericOptics u) v a b where
-    theOptic _ = Optics.Coerce.coerceS (gfield @name)
+-- instance GField name u v a b => RecordDotOptics name (GenericOptics u) v a b where
+--     theOptic _ = Optics.Coerce.coerceS (gfield @name)
 
 -- instance 
 --     GField (name :: Symbol) a a b b
@@ -42,3 +44,8 @@ instance GField name u v a b => RecordDotOptics name (GenericOptics u) v a b whe
 --     =>
 --     HasField name (Optic A_Lens NoIx s t u v) (Optic A_Lens NoIx s t a b) where
 --         getField = (% gfield @name)
+
+type SetField :: forall {k} . k -> Type -> Type -> Constraint
+class SetField x r a | x r -> a where
+  -- | Selector function to extract the field from the record.
+  setField :: a -> r -> r
--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main (main) where
 
+import Optics.Coerce
 import GHC.Generics
 import Optics.Core
 import Optics.Lens.Dot
@@ -14,17 +16,26 @@ data Whole a = Whole
   }
   deriving stock (Generic)
 
+instance GField name (Whole p) (Whole q) a b => RecordDotOptics name (Whole p) (Whole q) a b where
+    theOptic = gfield @name
+
 data Part a = Part
   { part1 :: Bool,
     subpart :: Subpart a
   }
   deriving stock (Generic)
 
+instance GField name (Part p) (Part q) a b => RecordDotOptics name (Part p) (Part q) a b where
+    theOptic = gfield @name
+
 data Subpart a = Subpart
   { subpart1 :: String,
     foo :: a
   }
   deriving stock (Generic)
+
+instance GField name (Subpart p) (Subpart q) a b => RecordDotOptics name (Subpart p) (Subpart q) a b where
+    theOptic = gfield @name
 
 whole :: Whole Int
 whole = Whole 0 (Part True (Subpart "wee" 7))
